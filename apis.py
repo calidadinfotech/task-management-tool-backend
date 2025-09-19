7aea15a71cc2e0adfddd09cd115bc56a26d7a50d
hdabhi-calidad <144206447+hdabhi-calidad@users.noreply.github.com>
2025-09-19 12:20:34 +0530

    Update apis.py

--- apis.py
+++ apis.py
@@ -28,7 +28,7 @@ with app.app_context():
 @app.route('/tasks', methods=['POST'])
 def create_task():
     data = request.get_json()
-    title = data.get('title..dd
+    title = data.get('title..jhgjh
     description = data.get('description', '')
     assignee = data.get('assignee', None)
 
