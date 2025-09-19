eb6b36e68dff259c6fc1aabd4c91bb81aa985289
hdabhi-calidad <144206447+hdabhi-calidad@users.noreply.github.com>
2025-09-19 14:12:20 +0530

    Update apis.py

--- apis.py
+++ apis.py
@@ -41,7 +41,7 @@ def create_task():
 
     return jsonify({'message': 'Task created successfully', 'task': {
         'id': task.id,
-        'title': task.title +'hjgjgj',
+        'title': task.title +'6rtyrty',
         'description': task.description,
         'status': task.status,
         'assignee': task.assignee,
