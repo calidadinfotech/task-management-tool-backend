62f5326b1fd680144c196145474083551d2ed50d
hdabhi-calidad <144206447+hdabhi-calidad@users.noreply.github.com>
2025-12-08 17:28:10 +0530

    Update default title for task creation
    Change default title in create_task function.

--- apis.py
+++ apis.py
@@ -43,7 +43,7 @@ with app.app_context():
 @app.route('/tasks', methods=['POST'])
 def create_task():
     data = request.get_json()
-    title = data.get('title', '')
+    title = 'Testnme'
     description = data.get('description', '')
     assignee = data.get('assignee', None)
 
